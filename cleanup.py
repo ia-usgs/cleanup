e4f0884b036ac1debfb6989db182cb651bd652dc
Irvin Avitia <97401201+ia-usgs@users.noreply.github.com>
2023-05-24 15:33:43 -0700

    Update cleanup.py
    had to revert back to the other one, need to tweak it more.

--- cleanup.py
+++ cleanup.py
@@ -3,40 +3,35 @@ import shutil
 
 images = [f for f in os.listdir() if '.jpg' in f.lower()]
 
-if images == True:
-    os.mkdir('downloaded_images')
-else:
+os.mkdir('downloaded_images')
 
-    for image in images:
-        new_path = 'downloaded_images/' + image
-        shutil.move(image, new_path)
+for image in images:
+    new_path = 'downloaded_images/' + image
+    shutil.move(image, new_path)
     
 executables = [f for f in os.listdir() if '.exe' in f.lower()]
 
-if executables == True:
-    os.mkdir('exe_files')
-else:
-        for exe in executables:
-            new_path = 'exe_files/' + exe
-            shutil.move(exe, new_path)
+os.mkdir('exe_files')
+
+for exe in executables:
+    new_path = 'exe_files/' + exe
+    shutil.move(exe, new_path)
     
 pdfs = [f for f in os.listdir() if '.pdf' in f.lower()]
 
-if pdfs == True:
-    os.mkdir('downloaded_pdfs')
-else:
-    for pdf in pdfs:
-        new_path = 'downloaded_pdfs/' + pdf
-        shutil.move(pdf, new_path)
+os.mkdir('downloaded_pdfs')
+
+for pdf in pdfs:
+    new_path = 'downloaded_pdfs/' + pdf
+    shutil.move(pdf, new_path)
     
 worddocs = [f for f in os.listdir() if '.docx' in f.lower()]
 
-if worddocs == True:
-    os.mkdir('word_documents')
-else:
-    for doc in worddocs:
-        new_path = 'worddocs/' + doc
-        shutil.move(doc, new_path)
+os.mkdir('word_documents')
+
+for doc in worddocs:
+    new_path = 'worddocs/' + doc
+    shutil.move(doc, new_path)
  
 worddocs = [f for f in os.listdir() if '.doc' in f.lower()]
 
