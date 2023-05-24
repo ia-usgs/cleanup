d751958536f63971c5d9bbd55bcedcf839016160
Irvin Avitia <97401201+ia-usgs@users.noreply.github.com>
2023-05-24 15:52:32 -0700

    Update cleanup.py
    fixed the previous issue

--- cleanup.py
+++ cleanup.py
@@ -1,42 +1,52 @@
 import os
 import shutil
 
-images = [f for f in os.listdir() if '.jpg' in f.lower()]
 
-os.mkdir('downloaded_images')
+if os.path.isdir('downloaded_images/') == False:
 
-for image in images:
-    new_path = 'downloaded_images/' + image
-    shutil.move(image, new_path)
+
+    images = [f for f in os.listdir() if '.jpg' in f.lower()]
+        
+    os.mkdir('downloaded_images')
+        
+    for image in images:
+        new_path = 'downloaded_images/' + image
+        shutil.move(image, new_path)
     
-executables = [f for f in os.listdir() if '.exe' in f.lower()]
+if os.path.isdir('exe_files/') == False:
 
-os.mkdir('exe_files')
-
-for exe in executables:
-    new_path = 'exe_files/' + exe
-    shutil.move(exe, new_path)
+    executables = [f for f in os.listdir() if '.exe' in f.lower()]
     
-pdfs = [f for f in os.listdir() if '.pdf' in f.lower()]
-
-os.mkdir('downloaded_pdfs')
-
-for pdf in pdfs:
-    new_path = 'downloaded_pdfs/' + pdf
-    shutil.move(pdf, new_path)
+    os.mkdir('exe_files')
     
-worddocs = [f for f in os.listdir() if '.docx' in f.lower()]
-
-os.mkdir('word_documents')
-
-for doc in worddocs:
-    new_path = 'worddocs/' + doc
-    shutil.move(doc, new_path)
- 
-worddocs = [f for f in os.listdir() if '.doc' in f.lower()]
-
-for doc in worddocs:
-    new_path = 'worddocs/' + doc
-    shutil.move(doc, new_path)
+    for exe in executables:
+        new_path = 'exe_files/' + exe
+        shutil.move(exe, new_path)
+    
+if os.path.isdir('downloaded_pdfs/') == False:
+    
+    pdfs = [f for f in os.listdir() if '.pdf' in f.lower()]
+    
+    os.mkdir('downloaded_pdfs')
+    
+    for pdf in pdfs:
+        new_path = 'downloaded_pdfs/' + pdf
+        shutil.move(pdf, new_path)
+    
+if os.path.isdir('word_documents/') == False:
+    
+    worddocs = [f for f in os.listdir() if '.docx' in f.lower()]
+    
+    os.mkdir('word_documents')
+    
+    for doc in worddocs:
+        new_path = 'worddocs/' + doc
+        shutil.move(doc, new_path)
+     
+    worddocs = [f for f in os.listdir() if '.doc' in f.lower()]
+    
+    for doc in worddocs:
+        new_path = 'worddocs/' + doc
+        shutil.move(doc, new_path)
     
     
