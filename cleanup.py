14c360262619e854e5f25ff9141003a41b5b6757
Irvin Avitia <97401201+ia-usgs@users.noreply.github.com>
2023-05-25 10:35:28 -0700

    Update cleanup.py
    Added an else statement that will still move stuff around even if the directory is still made.

--- cleanup.py
+++ cleanup.py
@@ -1,3 +1,8 @@
+#This script is made to clean up my downloads folder. Eventually I will
+#have it grow to where it can do more stuff
+
+
+
 import os
 import shutil
 
@@ -12,6 +17,14 @@ if os.path.isdir('downloaded_images/') == False:
     for image in images:
         new_path = 'downloaded_images/' + image
         shutil.move(image, new_path)
+        
+    images2 = [f for f in os.listdir() if '.png' in f.lower()]
+        
+    for image in images2:
+        new_path = 'downloaded_images/' + image
+        shutil.move(image, new_path)
+        
+    print('Created Images Directory')
     
 if os.path.isdir('exe_files/') == False:
 
@@ -22,6 +35,7 @@ if os.path.isdir('exe_files/') == False:
     for exe in executables:
         new_path = 'exe_files/' + exe
         shutil.move(exe, new_path)
+    print('Created Exe Directory')
     
 if os.path.isdir('downloaded_pdfs/') == False:
     
@@ -32,6 +46,7 @@ if os.path.isdir('downloaded_pdfs/') == False:
     for pdf in pdfs:
         new_path = 'downloaded_pdfs/' + pdf
         shutil.move(pdf, new_path)
+    print('Created PDFS Directory')
     
 if os.path.isdir('word_documents/') == False:
     
@@ -42,11 +57,54 @@ if os.path.isdir('word_documents/') == False:
     for doc in worddocs:
         new_path = 'worddocs/' + doc
         shutil.move(doc, new_path)
+    
      
-    worddocs = [f for f in os.listdir() if '.doc' in f.lower()]
+    worddocs2 = [f for f in os.listdir() if '.doc' in f.lower()]
+    
+    for doc in worddocs2:
+        new_path = 'worddocs/' + doc
+        shutil.move(doc, new_path)
+    print('Created WORD DOCS Directory')
+    
+else:
+    
+    images = [f for f in os.listdir() if '.jpg' in f.lower()]
+    
+    for image in images:
+        new_path = 'downloaded_images/' + image
+        shutil.move(image, new_path)
+        
+    images2 = [f for f in os.listdir() if '.png' in f.lower()]
+        
+    for image in images2:
+        new_path = 'downloaded_images/' + image
+        shutil.move(image, new_path)
+
+    
+    executables = [f for f in os.listdir() if '.exe' in f.lower()]
+    
+    for exe in executables:
+        new_path = 'exe_files/' + exe
+        shutil.move(exe, new_path)
+
+        
+    pdfs = [f for f in os.listdir() if '.pdf' in f.lower()]
+    
+    for pdf in pdfs:
+        new_path = 'downloaded_pdfs/' + pdf
+        shutil.move(pdf, new_path)
+        
+    
+    worddocs = [f for f in os.listdir() if '.docx' in f.lower()]
     
     for doc in worddocs:
         new_path = 'worddocs/' + doc
         shutil.move(doc, new_path)
     
+     
+    worddocs2 = [f for f in os.listdir() if '.doc' in f.lower()]
     
+    for doc in worddocs2:
+        new_path = 'worddocs/' + doc
+        shutil.move(doc, new_path)
+        
