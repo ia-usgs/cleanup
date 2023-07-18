d08e8a3774797c823f98b87f8b833b0fbc98dcba
Irvin Avitia <97401201+ia-usgs@users.noreply.github.com>
2023-07-18 14:19:03 -0700

    Update cleanup.py
    added 2 new functions. One to display what is in the directory, and a button to delete any file inside with the right permissions

--- cleanup.py
+++ cleanup.py
@@ -9,6 +9,7 @@
 import os
 import shutil
 import tkinter as tk
+from tkinter import messagebox, filedialog
 
 
 extensions_to_folders = {
@@ -57,19 +58,67 @@ def move_files_to_folder(folder, extensions):
         shutil.move(file, new_path)
         print(f'{file} is now moved to {new_path}')
 
+def delete_files(files):
+    failed_deletions = []
+    for file in files:
+        try:
+            os.remove(file)
+            print(f'{file} is deleted')
+        except PermissionError:
+            print(f'Permission denied: {file}')
+            failed_deletions.append(file)
+        except OSError:
+            print(f'Failed to delete: {file}')
+            failed_deletions.append(file)
+    
+    return failed_deletions
+
+def list_files():
+    file_text.delete(0, tk.END)  # Clear previous content
+    files = os.listdir()
+    for file in files:
+        file_text.insert(tk.END, file)
+
+def clean_files():
+    letsClean()
+    messagebox.showinfo("Cleanup", "Cleanup complete.")
+
 def letsClean():
     for extension, folder in extensions_to_folders.items():
         move_files_to_folder(folder, [extension])
 
-def clean_files():
-    letsClean()
-    print('Cleanup complete.')
+def delete_selected_files():
+    selected_files = file_text.curselection()
+    files_to_delete = [file_text.get(index) for index in selected_files]
+    if files_to_delete:
+        confirm = messagebox.askyesno("Confirmation", "Are you sure you want to delete the selected files?")
+        if confirm:
+            failed_deletions = delete_files(files_to_delete)
+            if failed_deletions:
+                failed_message = "Failed to delete the following files:\n\n" + "\n".join(failed_deletions)
+                messagebox.showwarning("Deletion Failed", failed_message)
+            else:
+                messagebox.showinfo("Deletion", "Selected files deleted successfully.")
+            list_files()
 
 def create_gui():
+    global file_text  # Access the global file_text variable
     top = tk.Tk()
-    top.geometry("200x125")
-    button = tk.Button(top, text="Clean", command=clean_files, borderwidth=2)
-    button.place(x=80, y=45)
+    top.geometry("300x300")
+
+    list_button = tk.Button(top, text="List Files", command=list_files)
+    list_button.pack(pady=10)
+
+    file_text = tk.Listbox(top, width=40, height=10, selectmode=tk.MULTIPLE)
+    file_text.pack()
+
+    delete_button = tk.Button(top, text="Delete Selected Files", command=delete_selected_files)
+    delete_button.pack(pady=10)
+
+    clean_button = tk.Button(top, text="Clean", command=clean_files)
+    clean_button.pack(pady=10)
+
     top.mainloop()
 
 create_gui()
+
